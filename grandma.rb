25a706fd093b1b37299a426c1ca74cc29bec75c6
Derek Cerretani <derekcerretani@gmail.com>
2018-12-17 20:15:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,6 +10,9 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
+
+speak_to_grandma(phrase)
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
